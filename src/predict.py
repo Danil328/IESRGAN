4c9685a57057445dd341eb89aba5580ce84b3b3d
Danil328 <Danil328@yandex.ru>
2019-04-21 22:16:07 +0300

    update

--- src/predict.py
+++ src/predict.py
@@ -1,25 +1,27 @@
+import json
+
 import cv2
-from math import log10
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-from PIL import Image
-import config
-from dataset.LRHR_Dataset import DatasetFromFolder
-from models.architecture import *
-from utils.pytorch_ssim import ssim
-from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+from models.architecture import *
 
 if __name__ == '__main__':
+	with open('config.json', 'r') as f:
+		config = json.load(f)
+
+	default_config = config['DEFAULT']
+	config = config['PREDICT']
+
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-	netG = RRDBNet(in_nc = 3, out_nc = 3, nf = 16, nb = 16, gc = 32, upscale = config.UPSCALE_FACTOR)
-	netG.load_state_dict(torch.load(f"output/models/netG_epoch={10}")['model_state_dict'])
+	netG = RRDBNet(in_nc = 3, out_nc = 3, nf = 16, nb = 16, gc = 32, upscale = default_config['upscale_factor'])
+	netG.load_state_dict(torch.load(config['path_to_model'])['model_state_dict'])
 	netG.to(device).eval()
 
-	path_to_image = ''
-	img = Image.open(path_to_image)
+	img = Image.open(config['path_to_image'])
 
 	with torch.no_grad():
 		img_tensor = ToTensor()(Image.open(img)).unsqueeze(0).to(device)
@@ -31,8 +33,8 @@ if __name__ == '__main__':
 	# (width,height)
 	image = cv2.imread("cell.png")  # your image path
 	tmp = image  # for drawing a rectangle
-	stepSize = 50
-	(w_width, w_height) = (50, 50)  # window size
+	stepSize = default_config['crop_size']
+	(w_width, w_height) = (default_config['crop_size'], default_config['crop_size'])  # window size
 	for x in range(0, image.shape[1] - w_width, stepSize):
 		for y in range(0, image.shape[0] - w_height, stepSize):
 			window = image[x:x + w_width, y:y + w_height, :]
